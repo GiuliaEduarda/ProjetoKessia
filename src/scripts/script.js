c15d5738c7895194a1f1143eecba9fa77303b1de
giulia <eduardagiulia@gmail.com>
2024-09-13 12:22:38 -0300

    first commit

---
+++ src/scripts/script.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('#sobremim', {
+    origin: 'left',
+    duration: 2000,
+    distance: '20%',
+    
+})
+revelar.reveal('#img2', {
+    origin: 'right',
+    duration: 2000,
+    distance: '20%',
+    delay: 500
+})
+revelar.reveal('#projetos', {
+    
+    duration: 2000,
+    distance: '20%'
+})
+revelar.reveal('#projetos', {
+    
+    duration: 2000,
+    distance: '20%'
+})
+
+revelar.reveal('#butt2', {
+    
+    duration: 1000,
+    distance: '20%'
+})
